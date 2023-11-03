8e8997b61467832cb18cd34b10973c1ef23feeda
KyumKyum <mycheesepasta@gmail.com>
2023-11-03 14:47:18 +0900

    💫 Feature: Add Exception Handler to handleSyncRequest

--- src/main/kotlin/com/holme/be_app/api/sync/controller/SyncController.kt
+++ src/main/kotlin/com/holme/be_app/api/sync/controller/SyncController.kt
@@ -3,8 +3,11 @@ package com.holme.be_app.api.sync.controller
 import com.holme.be_app.api.sync.entity.Instance
 import com.holme.be_app.api.sync.entity.SingleSyncRequest
 import com.holme.be_app.api.sync.entity.SyncRequest
+import com.holme.be_app.api.sync.entity.SyncResponse
 import com.holme.be_app.api.sync.factory.SyncInstanceTypeFactory
 import com.holme.be_app.api.sync.service.SyncRequestService
+import com.holme.be_app.entity.response.SingleResponse
+import com.holme.be_app.entity.response.SingleResponseService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -15,17 +18,24 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/api/v1/sync")
 class SyncController(
     @Autowired private val syncRequestService: SyncRequestService<Instance>,
-    @Autowired private val syncInstanceTypeFactory: SyncInstanceTypeFactory
+    @Autowired private val syncInstanceTypeFactory: SyncInstanceTypeFactory,
+    @Autowired private val responseService: SingleResponseService<SyncResponse>
 ) {
     @PostMapping("/request")
-    fun handleSyncRequest(@RequestBody syncRequest: SyncRequest<Instance>) {
-        val requestPayloads = syncRequest.payloads
-        for (request: SingleSyncRequest<Instance> in requestPayloads) {
-            //* Handle & Send every request received
-            val type = request.instanceType
-            val instance = syncInstanceTypeFactory.generateInstanceClass(type)
-            syncRequestService.sendSyncRequest(type, instance)
+    fun handleSyncRequest(@RequestBody syncRequest: SyncRequest<Instance>): SingleResponse<SyncResponse> {
+        try{
+            val requestPayloads = syncRequest.payloads
+            for (request: SingleSyncRequest<Instance> in requestPayloads) {
+                //* Handle & Send every request received
+                val type = request.instanceType
+                val instance = syncInstanceTypeFactory.generateInstanceClass(type)
+                syncRequestService.sendSyncRequest(type, instance)
+            }
+            // TODO: Add Response Type
+        }catch (e: Error) {
+            val message: String = if(e.message is String) e.message!! else e.toString()
+            return responseService.isFailure(-1, message)
         }
-    // TODO: Add Response Type
+        return responseService.isSuccessful()
     }
 }
---
+++ src/main/kotlin/com/holme/be_app/api/sync/entity/SyncResponse.kt
@@ -0,0 +1,6 @@
+package com.holme.be_app.api.sync.entity
+
+class SyncResponse(
+    val ok: Boolean,
+    val message: String?
+)
